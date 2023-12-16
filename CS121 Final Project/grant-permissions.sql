8ff3f26258c1a6ea82389b8429bd63e4f637a99d
favourokodogbe <89471025+favourokodogbe@users.noreply.github.com>
2023-12-15 21:48:44 -0500

    Delete CS121 Final Project directory
    Signed-off-by: favourokodogbe <89471025+favourokodogbe@users.noreply.github.com>

--- CS121 Final Project/grant-permissions.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER 'dog_admin'@'localhost' IDENTIFIED BY 'dog_admin';
-CREATE USER 'dog_client'@'localhost' IDENTIFIED BY 'dog_client';
--- Can add more users or refine permissions
-GRANT ALL PRIVILEGES ON adoptdb.* TO 'dog_admin'@'localhost';
-GRANT SELECT ON adoptdb.* TO 'dog_client'@'localhost';
-FLUSH PRIVILEGES;
